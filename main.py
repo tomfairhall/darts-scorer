f37410f857c6b382b6abf64e0b329b5e3fcf2dc4
Tom <thomasjackfairhall@gmail.com>
2022-07-25 21:44:47 +1000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Darts Scorer v0.1.0
-# 
+#         if throw[0] == "t":
+#            score_round += 3*(int(throw.split("t")[1]))
+#        elif throw[0] == "d":
+ #           score_round += 2*(int(throw.split("d")[1]))
 #
 # Valid inputs 0 - 20, d1 - d20, t1 - t20, sb, db
 
@@ -17,8 +20,31 @@ print("Welcome to darts scorer v0.1!")
 print(args.home, "v.", args.away)
 print("Starting points:", args.score)
 
-def busted(score_hit, score_remaining):
-    if 
+def check_busted(score_hit, score_remaining):
+    if score_hit - score_remaining < 0:
+        print("Busted!")
+        return True
+    else:
+        return False
+
+# how to check if scores are right... regrex?
+def check_score(throws):
+    score_round = 0
+
+    for throw in throws:
+        if throw[0] == "t":
+            continue
+        elif throw[0] == "d":
+            continue
+        elif throw == "sb":
+            continue
+        elif throw == "db":
+            continue
+        elif int(throw) >= 0 or int(throw) <= 20:
+            continue
+        else:
+            print("Please input valid scores!")
+            return False
 
 class Player():
     def __init__(self, name, score):
@@ -31,19 +57,10 @@ class Player():
         if len(throws) > 3 or len(throws) < 3:
             print("Enter only 3 throws!")
         else:
-            for throw in throws:
-                if throw[0] == "t":
-                    self.score -= 3*(int(throw.split("t")[1]))
-                elif throw[0] == "d":
-                    self.score -= 2*(int(throw.split("d")[1]))
-                elif throw == "sb":
-                    self.score -= 25
-                elif throw == "db":
-                    self.score -= 50
-                elif int(throw) >= 0 or int(throw) <= 20:
-                    self.score -= int(throw)
-                else:
-                    print("Please input valid scores!")
+            check_score()
+            check_busted()
+
+            
         print(self.name + "'s", "score:", self.score)
 
 home = Player(args.home, int(args.score))
